08fc6a423b9d110c2b0fb0752665f6e396dc4a8c
Abubakaryounis97 <Abubakaryounis97@gmail.com>
2025-09-06 22:35:17 -0400

    Changes in pollController

--- src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controller/PollController.java
@@ -6,7 +6,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.zipcoder.tc_spring_poll_application.domain.Poll;
@@ -59,7 +66,7 @@ public class PollController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    // DELETE /polls/{pollId} - Delete a poll
+
     @DeleteMapping("/{pollId}")
     public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
         pollRepository.deleteById(pollId);
